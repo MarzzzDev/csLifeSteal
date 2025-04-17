07a03588a34b6bdd12040c1946d6b9996db1c53d
MarzzzDEV <marseceva@gmail.com>
2025-04-17 23:38:06 +0300

    dolbaeb i had 2 of using CounterStrikeSharp.API.Modules.Entities; and caused an annoying warning

--- LifeSteal.cs
+++ LifeSteal.cs
@@ -7,7 +7,6 @@ using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Utils;
 using CounterStrikeSharp.API.Modules.Events;
-using CounterStrikeSharp.API.Modules.Entities;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
